e36887b14b8ee345d20e55333e7e3789f18efe22
Kyeongmo Noh <kyeongmo2@gmail.com>
2021-07-24 14:02:41 +0900

    react hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.scss'
+
+import { useState, useRef } from 'react'
+
 // import Cats from './components/Cats'
 import Counter from './components/Counter'
 
 function App() {
+  const [fillNumber, setFillNumber] = useState(1)
+  const counterRef = useRef(null)
+
+  const handleClick = () => {
+    setFillNumber(previousNumber => previousNumber + 1)
+  }
+
   return (
     <div className="App">
       <main>
-        <Counter />
+        <Counter fillNumber={fillNumber} ref={counterRef} />
+        <button onClick={handleClick}>Plus FillNumber 1</button>
       </main>
     </div>
   )
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,25 +1,82 @@
-import { useState } from 'react'
+import { useState, useMemo, memo, forwardRef } from 'react'
 
-function getCount() {
-  console.log('expensive')
+// function getCount() {
+//   console.log('expensive')
+//   return Array(100000)
+//     .fill(1)
+//     .reduce((acc, cur) => acc + cur, 0)
+// }
+
+// function Counter() {
+//   const [count, setCount] = useState(() => getCount())
+
+//   const handleClick = () => {
+//     setCount((previousCount) => previousCount + 1)
+//   }
+
+//   return (
+//     <section>
+//       <span>{count}</span>
+//       <button onClick={handleClick}>Plus 1</button>
+//     </section>
+//   )
+// }
+
+function getCount(fillNumber) {
+  console.log('An expensive work is been executing...')
+
   return Array(100000)
-    .fill(1)
+    .fill(fillNumber)
     .reduce((acc, cur) => acc + cur, 0)
 }
 
-function Counter() {
-  const [count, setCount] = useState(() => getCount())
+// const a = {
+//   teacher: 'john',
+//   mother: 'emily',
+//   // 변화의 가능성
+// }
+// const b = {
+//   teacher: 'john',
+//   mother: 'emily',
+// }
+// a === b // shallow compare
 
-  const handleClick = () => {
-    setCount((previousCount) => previousCount + 1)
-  }
+function isSameObject(first, second) {
+  // return Object.entries(first).every(([key, value]) => {
+  //   if (value === second[key]) {
+  //     return true
+  //   }
+  // })
+  return JSON.stringify(first) === JSON.stringify(second)
+}
+// console.log(isSameObject(a, b))
+
+// function Counter({ fillNumber }) {
+//   const [text, setText] = useState('')
+//   const sum = useMemo(() => getCount(fillNumber), [fillNumber])
+
+//   return (
+//     <section>
+//       <span>Sum: {sum}</span>
+//       <input value={text} type="text" onChange={event => setText(event.target.value)} />
+//     </section>
+//   )
+// }
+
+const Counter = forwardRef(({ fillNumber }, ref) => {
+  const [text, setText] = useState('')
+  const sum = useMemo(() => getCount(fillNumber), [fillNumber])
 
   return (
     <section>
-      <span>{count}</span>
-      <button onClick={handleClick}>Plus 1</button>
+      <span>Sum: {sum}</span>
+      <input value={text} ref={ref} type="text" onChange={event => setText(event.target.value)} />
     </section>
   )
-}
+})
 
-export default Counter
+Counter.displayName = 'Counter'
+
+export default memo(Counter, (prevProps, nextProps) => {
+  return isSameObject(prevProps, nextProps)
+})
